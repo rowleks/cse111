8164122c35f98c12732f051256db3f47a7299c0c
rowleks <momohrowland@gmail.com>
2025-05-18 15:06:38 +0100

    water pressure program v2

--- test_water_flow.py
+++ test_water_flow.py
@@ -2,6 +2,9 @@ from water_flow import (
     water_column_height,
     pressure_gain_from_water_height,
     pressure_loss_from_pipe,
+    pressure_loss_from_fittings,
+    reynolds_number,
+    pressure_loss_from_pipe_reduction, psi_from_kpa
 )
 import pytest
 from pytest import approx
@@ -42,6 +45,39 @@ def test_pressure_loss_from_pipe():
     assert pressure_loss_from_pipe(0.286870, 1800.75, 0.013, 1.65) == approx(
         -110.884, abs=0.001
     )
+
+
+def test_pressure_loss_from_fittings():
+    assert pressure_loss_from_fittings(0.00, 3) == approx(0.000, abs=0.001)
+    assert pressure_loss_from_fittings(1.65, 0) == approx(0.000, abs=0.001)
+    assert pressure_loss_from_fittings(1.65, 2) == approx(-0.109, abs=0.001)
+    assert pressure_loss_from_fittings(1.75, 2) == approx(-0.122, abs=0.001)
+    assert pressure_loss_from_fittings(1.75, 5) == approx(-0.306, abs=0.001)
+
+
+def test_reynolds_number():
+    assert reynolds_number(0.048692, 0.00) == approx(0, abs=1)
+    assert reynolds_number(0.048692, 1.65) == approx(80069, abs=1)
+    assert reynolds_number(0.048692, 1.75) == approx(84922, abs=1)
+    assert reynolds_number(0.286870, 1.65) == approx(471729, abs=1)
+    assert reynolds_number(0.286870, 1.75) == approx(500318, abs=1)
+
+
+def test_pressure_loss_from_pipe_reduction():
+    assert pressure_loss_from_pipe_reduction(0.28687, 0.00, 1, 0.048692) == approx(
+        0.000, abs=0.001
+    )
+    assert pressure_loss_from_pipe_reduction(0.28687, 1.65, 471729, 0.048692) == approx(
+        -163.744, abs=0.001
+    )
+    assert pressure_loss_from_pipe_reduction(0.28687, 1.75, 500318, 0.048692) == approx(
+        -184.182, abs=0.001
+    )
+
+def test_psi_from_kpa():
+    assert psi_from_kpa(0.0) == approx(0.0, abs=0.001)
+    assert psi_from_kpa(101.325) == approx(14.6959, abs=0.001)
+    assert psi_from_kpa(200.0) == approx(29.0076, abs=0.001)
 
 
 pytest.main([__file__, "-v", "--tb=line"])
